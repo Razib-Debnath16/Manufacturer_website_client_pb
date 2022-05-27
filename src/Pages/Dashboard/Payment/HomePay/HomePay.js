99722ad4f431f554fe6a17e14e922d5bb083c82f
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-05-28 03:27:53 +0600

    updating

--- src/Pages/Dashboard/Payment/HomePay/HomePay.js
+++ src/Pages/Dashboard/Payment/HomePay/HomePay.js
@@ -5,10 +5,10 @@ import img2 from '../../../../Assets/Images/Payment/credit card2.jpg';
 const HomePay = () => {
     return (
         <div>
-            <h2 className='text-secondary text-center text-2xl'>Payment Method</h2>
-            <div class="hero min-h-screen bg-base-200">
+            <h2 className='text-secondary text-center text-2xl'>Payments Method</h2>
+            <div class="hero  bg-base-200">
                 <div class="hero-content flex-col lg:flex-row">
-                    <img src={img1} class="max-w-sm rounded-lg shadow-2xl" alt='' />
+                    <img src={img1} class="max-w-sm rounded-lg shadow-2xl px-2" alt='' />
                     <div>
                         <img src={img2} class="max-w-sm rounded-lg shadow-2xl" alt='' />
                     </div>
